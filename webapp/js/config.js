bcdd9511db2769a3659dcf14fbafe1b47b38fb8b
thenguyen-farmer <thenguyen.farmer@gmail.com>
2025-12-29 23:42:55 +0700

    Update config.js

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -4,10 +4,10 @@
 // 2. Vào Supabase Dashboard -> Authentication -> URL Configuration -> Site URL.
 
 const CONFIG = {
-    SUPABASE_URL = 'https://aybopfqltybsfbqpxrsu.supabase.co',
-    SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Ym9wZnFsdHlic2ZicXB4cnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5OTM0ODgsImV4cCI6MjA4MjU2OTQ4OH0.O9Su4haSDqay3jfjz7SYUab3bPQ2TzX4YGH9omlWj34',
-    GEMINI_API_KEY = 'AIzaSyBEVOIN-KkGaT08PWUF9ywOHFyWOhb0i6A', // Placeholder
-    WEATHER_API_KEY = 'd55adcde720c023c53ec16f44ea51a06', // Lấy tại makersuite.google.com
+    SUPABASE_URL: 'https://aybopfqltybsfbqpxrsu.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5Ym9wZnFsdHlic2ZicXB4cnN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY5OTM0ODgsImV4cCI6MjA4MjU2OTQ4OH0.O9Su4haSDqay3jfjz7SYUab3bPQ2TzX4YGH9omlWj34',
+    OPENWEATHER_KEY: 'd55adcde720c023c53ec16f44ea51a06',
+    GEMINI_KEY: 'AIzaSyCNEiW7Lx5Cz4xl1Sm8L2HY9ksJq0nYKGs', // Lấy tại makersuite.google.com
     
     // Cấu hình Bản đồ mặc định (Google Hybrid)
     MAP_TILE_URL: 'https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}',
